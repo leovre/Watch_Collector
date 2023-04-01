8cf38b201a25ad61affbe35a3eabbd6d19d6640d
Leo <leonardovrodriguez246@gmail.com>
2023-04-01 00:47:49 -0400

    view all watches route available

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
 
+watches = [
+    {'brand': 'Rolex', 'name': 'submariner', 'manufactured': 2005,
+        'img': 'https://imageio.forbes.com/b-i-forbesimg/arieladams/files/2013/09/Rolex-SUBMARINER-NoDATE-047.jpg?format=jpg&width=400'},
+    {'brand': 'Casio', 'name': 'Vintage', 'manufactured': 2012,
+        'img': 'https://www.mastersintime.com/pictures/casio-classic-edgy-a700wem-7aef-10482584.jpg'},
+    {'brand': 'G-Shock', 'name': 'CasiOak', 'manufactured': 2008,
+        'img': 'https://wornandwound.com/library/uploads/2022/08/GM-B2100D-1A_front-scaled.jpg'}
+
+]
+
 # Create your views here.
+
+
 def home(request):
-    
+    return render(request, 'home.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def watches_index(request):
+    return render(request, 'watches/index.html', {
+        'watches': watches
+    })
--- watch_collector/settings.py
+++ watch_collector/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'watch_collector.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'watchcollector',
+        'NAME': 'watchcollector',
     }
 }
 
